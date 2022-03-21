ce09fc0b34ec9402ca0f05d6d0b14a4bd68f0df5
Ben Combs <thecombs.ben@gmail.com>
2022-03-21 16:32:13 -0600

    Remove credits, will re-add later

--- include/model/assets.php
+++ include/model/assets.php
@@ -1,17 +1,4 @@
 <?php
-/*********************************************************************
-users.php
-
-Peter Rotich <peter@osticket.com>
-Jared Hancock <jared@osticket.com>
-Copyright (c)  2006-2014 osTicket
-http://www.osticket.com
-
-Released under the GNU General Public License WITHOUT ANY WARRANTY.
-See LICENSE.TXT for details.
-
-vim: expandtab sw=4 ts=4 sts=4:
- **********************************************************************/
 global $ost;
 global $cfg;
 require('staff.inc.php');
