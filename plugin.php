9deda88158039c4e67fe75af5a4e91d2e23e69a9
Ben Combs <thecombs.ben@gmail.com>
2022-03-23 14:19:14 -0600

    Update version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 set_include_path(get_include_path().PATH_SEPARATOR.dirname(__file__).'/include');
 return array(
     'bcombs18:inventory',
-    'version' => '0.9-alpha.4',
+    'version' => '0.9-alpha.4.2',
     'name' => 'Inventory Manager',
     'author' => 'bcombs18',
     'description' => 'Inventory Asset Management',
