c5d6e516ab8abc09bde43e4222984b2187dd9abb
Ben Combs <thecombs.ben@gmail.com>
2022-09-19 16:09:05 -0600

    Add phone controller

---
+++ include/controller/Phone.php
@@ -0,0 +1,371 @@
+<?php
+
+namespace controller;
+
+require_once(INCLUDE_DIR . 'class.staff.php');
+require_once(INCLUDE_DIR . 'class.orm.php');
+require_once(INCLUDE_DIR . 'class.ajax.php');
+require_once(INVENTORY_MODEL_DIR . 'AssetSearch.php');
+
+use Format;
+use \AssetAdhocSearch;
+use model\AssetForm;
+use \AssetSavedSearch;
+use User;
+
+class Phone extends \AjaxController {
+
+    function lookup() {
+        global $thisstaff;
+
+        $limit = isset($_REQUEST['limit']) ? (int) $_REQUEST['limit']:25;
+        $phones=array();
+        // Bail out of query is empty
+        if (!$_REQUEST['q'])
+            return $this->json_encode($phones);
+
+        $hits = \model\Phone::objects()
+            ->values('phone_model', 'phone_number', 'sim', 'imei', 'color')
+            ->order_by(\SqlAggregate::SUM(new \SqlCode('Z1.relevance')), \QuerySet::DESC)
+            ->distinct('phone_id')
+            ->limit($limit);
+
+        $q = $_REQUEST['q'];
+
+        if (strlen(\Format::searchable($q)) < 3)
+            return $this->encode(array());
+
+        $searcher = new \AssetMysqlSearchBackend();
+        $hits = $searcher->find($q, $hits, false);
+
+        if (!count($hits) && preg_match('`\w$`u', $q)) {
+            // Do wild-card fulltext search
+            $_REQUEST['q'] = $q.'*';
+            return $this->lookup();
+        }
+
+        $phones = array_values($phones);
+
+        return $this->json_encode($phones);
+    }
+
+    function addPhone() {
+        global $thisstaff;
+
+        $info = array();
+
+        if (!\AuthenticationBackend::getSearchDirectories())
+            $info['lookup'] = 'local';
+
+        if ($_POST) {
+            if (!$thisstaff->hasPerm(\User::PERM_CREATE))
+                \Http::response(403, 'Permission Denied');
+
+            $info['title'] = __('Add New Phone');
+            $form = \model\PhoneForm::getPhoneForm()->getForm($_POST);
+            if (($phone = \model\Phone::fromForm($form)))
+                \Http::response(201, $phone->to_json(), 'application/json');
+
+            $info['error'] = sprintf('%s - %s', __('Error adding phone'), __('Please try again!'));
+        }
+
+        return self::_lookupform($form, $info);
+    }
+
+    function editPhone($id) {
+        global $thisstaff;
+
+        if(!$thisstaff)
+            \Http::response(403, 'Login Required');
+        elseif (!$thisstaff->hasPerm(User::PERM_EDIT))
+            \Http::response(403, 'Permission Denied');
+        elseif(!($phone = \model\Phone::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        $info = array(
+            'title' => sprintf(__('Update %s'), Format::htmlchars($phone->getModel()))
+        );
+        $forms = $phone->getForms();
+
+        include(INVENTORY_VIEWS_DIR . 'phone.tmpl.php');
+    }
+
+    function updatePhone($id) {
+        global $thisstaff;
+
+        if(!$thisstaff)
+            \Http::response(403, 'Login Required');
+        elseif (!$thisstaff->hasPerm(User::PERM_EDIT))
+            \Http::response(403, 'Permission Denied');
+        elseif(!($phone = \model\Asset::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        $errors = array();
+        $form = PhoneForm::getPhoneForm()->getForm($_POST);
+
+        if ($phone->updateInfo($_POST, $errors, true) && !$errors)
+            \Http::response(201, $phone->to_json(),  'application/json');
+
+        $forms = $phone->getForms();
+        include(INVENTORY_VIEWS_DIR . 'phone.tmpl.php');
+    }
+
+    function delete($id) {
+        global $thisstaff;
+
+        if (!$thisstaff)
+            \Http::response(403, 'Login Required');
+        elseif (!$thisstaff->hasPerm(User::PERM_DELETE))
+            \Http::response(403, 'Permission Denied');
+        elseif (!($phone = \model\Phone::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        $info = array();
+        if ($_POST) {
+            if (!$info['error'] && $phone->delete())
+                \Http::response(204, 'Phone deleted successfully');
+            elseif (!$info['error'])
+                $info['error'] = sprintf('%s - %s', __('Unable to delete phone'), __('Please try again!'));
+        }
+
+        include(INVENTORY_VIEWS_DIR . 'deletePhone.tmpl.php');
+    }
+
+    function retire($id) {
+        global $thisstaff;
+
+        if (!$thisstaff)
+            \Http::response(403, 'Login Required');
+        elseif (!$thisstaff->hasPerm(User::PERM_DELETE))
+            \Http::response(403, 'Permission Denied');
+        elseif (!($phone = \model\Phone::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        $info = array();
+        if ($_POST) {
+            if (!$info['error'] && $phone->retire())
+                \Http::response(204, 'Phone retired successfully');
+            elseif (!$info['error'])
+                $info['error'] = sprintf('%s - %s', __('Unable to retire phone'), __('Please try again!'));
+        }
+
+        include(INVENTORY_VIEWS_DIR . 'retirePhone.tmpl.php');
+    }
+
+    function activate($id) {
+        global $thisstaff;
+
+        if (!$thisstaff)
+            \Http::response(403, 'Login Required');
+        elseif (!$thisstaff->hasPerm(User::PERM_DELETE))
+            \Http::response(403, 'Permission Denied');
+        elseif (!($phone = \model\Phone::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        $info = array();
+        if ($_POST) {
+            if (!$info['error'] && $phone->activate())
+                \Http::response(204, 'Phone activated successfully');
+            elseif (!$info['error'])
+                $info['error'] = sprintf('%s - %s', __('Unable to activate phone'), __('Please try again!'));
+        }
+
+        include(INVENTORY_VIEWS_DIR . 'activatePhone.tmpl.php');
+    }
+
+    function getPhone($id=false) {
+
+        if(($phone=\model\Phone::lookup(($id) ? $id : $_REQUEST['id'])))
+            \Http::response(201, $phone->to_json(), 'application/json');
+
+        $info = array('error' => sprintf(__('%s: Unknown or invalid ID.'), _N('phone', 'phones', 1)));
+
+        return self::_lookupform(null, $info);
+    }
+
+    function preview($id) {
+        global $thisstaff;
+
+        if(!$thisstaff)
+            \Http::response(403, 'Login Required');
+        elseif(!($phone = \model\Phone::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        $info = array(
+            'title' => '',
+            'phoneedit' => sprintf('#phone/%d/edit', $phone->getId()),
+        );
+        ob_start();
+        echo sprintf('<div style="width:650px; padding: 2px 2px 0 5px;"
+                id="u%d">', $phone->getId());
+        include(INVENTORY_VIEWS_DIR . 'phone.tmpl.php');
+        echo '</div>';
+        $resp = ob_get_contents();
+        ob_end_clean();
+
+        return $resp;
+
+    }
+
+    static function _lookupform($form=null, $info=array()) {
+        global $thisstaff;
+
+        if (!$info or !$info['title']) {
+            if ($thisstaff->hasPerm(User::PERM_CREATE))
+                $info += array('title' => __('Lookup or create a phone'));
+            else
+                $info += array('title' => __('Lookup a phone'));
+        }
+
+        ob_start();
+        include(INVENTORY_VIEWS_DIR . 'addPhone.php');
+        $resp = ob_get_contents();
+        ob_end_clean();
+        return $resp;
+    }
+
+    function handle() {
+        require_once INVENTORY_INCLUDE_DIR.'model/phones.php';
+    }
+
+    function viewUser($phone_id) {
+        global $thisstaff;
+
+        if(!$thisstaff
+            || !$phone= \model\Phone::lookup($phone_id))
+            \Http::response(404, 'No such asset');
+
+        $user = \User::lookup($phone->getAssigneeID());
+
+        if($user) {
+            $file = 'user.tmpl.php';
+            $info = array(
+                'title' => sprintf(__('%s: %s'), $phone->getModel(),
+                    Format::htmlchars($user->getName()))
+            );
+        } else {
+            $file = 'user-lookup.tmpl.php';
+            $info = array(
+                'title' => sprintf(__('%s: Unassigned'), $phone->getModel())
+            );
+        }
+
+        ob_start();
+        include('class.note.php');
+        include(INVENTORY_VIEWS_DIR.$file);
+        $resp = ob_get_contents();
+        ob_end_clean();
+        return $resp;
+
+    }
+
+    function changeUserForm($phone_id) {
+        global $thisstaff;
+
+        if(!$thisstaff
+            || !($phone=\model\Phone::lookup($phone_id)))
+            \Http::response(404, 'No such phone');
+
+
+        $user = \User::lookup($phone->getAssigneeID());
+
+        $info = array(
+            'title' => sprintf(__('Change user for asset %s'), $phone->getModel())
+        );
+
+        return self::_userlookup($user, null, $info);
+    }
+
+    static function _userlookup($user, $form, $info) {
+        global $thisstaff;
+
+        ob_start();
+        include(INVENTORY_VIEWS_DIR . 'user-lookup.tmpl.php');
+        $resp = ob_get_contents();
+        ob_end_clean();
+        return $resp;
+
+    }
+
+    function getUser($id=false) {
+
+        if(($user=\User::lookup(($id) ? $id : $_REQUEST['id'])))
+            \Http::response(201, $user->to_json(), 'application/json');
+
+        $info = array('error' => sprintf(__('%s: Unknown or invalid ID.'), _N('end user', 'end users', 1)));
+
+        return self::_lookupUserForm(null, $info);
+    }
+
+    function selectUser($id) {
+        global $thisstaff;
+
+        if ($id)
+            $user = \User::lookup($id);
+
+        $info = array('title' => __('Select User'));
+
+        ob_start();
+        include(INVENTORY_VIEWS_DIR . 'user-lookup.tmpl.php');
+        $resp = ob_get_contents();
+        ob_end_clean();
+        return $resp;
+
+    }
+
+    static function _lookupUserForm($form=null, $info=array()) {
+        global $thisstaff;
+
+        if (!$info or !$info['title']) {
+            if ($thisstaff->hasPerm(\User::PERM_CREATE))
+                $info += array('title' => __('Lookup or create a user'));
+            else
+                $info += array('title' => __('Lookup a user'));
+        }
+
+        ob_start();
+        include(INVENTORY_VIEWS_DIR . 'user-lookup.tmpl.php');
+        $resp = ob_get_contents();
+        ob_end_clean();
+        return $resp;
+    }
+
+    function lookupUser() {
+        return self::addUser();
+    }
+
+    static function addUser() {
+        global $thisstaff;
+
+        $info = array();
+
+        if (!\AuthenticationBackend::getSearchDirectories())
+            $info['lookup'] = 'local';
+
+        if ($_POST) {
+            if (!$thisstaff->hasPerm(User::PERM_CREATE))
+                \Http::response(403, 'Permission Denied');
+
+            $info['title'] = __('Add New User');
+            $form = \UserForm::getUserForm()->getForm($_POST);
+            if (!is_string($form->getField('name')->getValue()))
+                \Http::response(404, 'Invalid Data');
+            if (($user = User::fromForm($form)))
+                \Http::response(201, $user->to_json(), 'application/json');
+
+            $info['error'] = sprintf('%s - %s', __('Error adding user'), __('Please try again!'));
+        }
+
+        return self::_lookupUserForm($form, $info);
+    }
+
+    function createNote($id) {
+        if (!($phone = \model\Phone::lookup($id)))
+            \Http::response(404, 'Unknown phone');
+
+        require_once INCLUDE_DIR . 'class.ajax.php';
+        require_once INCLUDE_DIR . 'ajax.note.php';
+        $ajax = new \NoteAjaxAPI();
+        return $ajax->createNote('IP'.$id);
+    }
+}
