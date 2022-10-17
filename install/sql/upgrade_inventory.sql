37fe2c3feaf4d4a158a90d3f4c1fd7ebe147b9c6
Ben Combs <thecombs.ben@gmail.com>
2022-10-17 13:22:23 -0600

    Update version

--- install/sql/upgrade_inventory.sql
+++ install/sql/upgrade_inventory.sql
@@ -1,3 +1,3 @@
-UPDATE `%TABLE_PREFIX%plugin` SET version = '1.1.5' WHERE `name`='Inventory Manager'$
+UPDATE `%TABLE_PREFIX%plugin` SET version = '1.1.6' WHERE `name`='Inventory Manager'$
 
 ALTER TABLE `%TABLE_PREFIX%api_key` ADD `can_create_assets` int(1)$
